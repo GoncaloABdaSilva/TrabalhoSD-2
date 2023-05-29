7c2bc7f5fc6b1f96bde2ea6b57fb3d76b9333a2a
Gonçalo Silva <gab.silva@campus.fct.unl.pt>
2023-05-29 22:35:34 +0100

    Inicio proxy

---
+++ sd2223-trab2/src/sd2223/trab2/servers/rest/RestFeedsResourceProxy.java
@@ -0,0 +1,14 @@
+package sd2223.trab2.servers.rest;
+
+import jakarta.inject.Singleton;
+import sd2223.trab2.api.java.Feeds;
+import sd2223.trab2.mastodon.Mastodon;
+
+
+@Singleton
+public class RestFeedsResourceProxy extends RestFeedsResource<Feeds>{
+
+    public RestFeedsResourceProxy() {
+        super(Mastodon.getInstance());
+    }
+}
